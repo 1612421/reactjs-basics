94f64ea08f19e994e37d98e1c69b2441526ac861
1612421 <nguyenngocnghia53@gmail.com>
2019-05-31 15:18:17 +0700

    Passing Data with Props

--- src/app/index.js
+++ src/app/index.js
@@ -6,6 +6,11 @@ import { Home } from './components/home';
 
 class App extends React.Component {
     render() {
+        const user = {
+            name: 'Nguyễn Ngọc Nghĩa',
+            hobbies: ['Game', 'film', 'coding']
+        };
+
         return (
             <div className="container">
                 <div className="row">
@@ -13,9 +18,12 @@ class App extends React.Component {
                         <Header/>
                     </div>
                 </div>
+                <br/>
                 <div className="row">
                     <div className="col-10 offset-1">
-                        <Home/>
+                        <Home name={"Nghĩa"} age={21} user={user}>
+                            <p>This is paragraph!</p>
+                        </Home>
                     </div>
                 </div>
             </div>
